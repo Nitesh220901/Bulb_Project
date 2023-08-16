585f59709fc5a69ec13bd1fe38992068a407a48c
Nitesh220901 <80700918+Nitesh220901@users.noreply.github.com>
2023-08-15 22:16:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+// let onBulb = document.getElementById("on")
+// let offBulb = document.getElementById("off")
+
+
+
+function onBulb(){
+    let offImage = document.getElementById("off-image")
+
+    offImage.src = "images/on.jpeg"
+}
+
+
+function offBulb(){
+    let onImage = document.getElementById("off-image")
+
+    onImage.src = "images/off.jpeg"
+}
